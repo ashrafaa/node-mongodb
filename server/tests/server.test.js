8821b9ca54daf838aca7f7cc8fb209770e3be571
Ashraf Anwar <ashraf.a@spartadigital.co.uk>
2018-09-06 11:40:41 +0100

    add patch route and test cases for /todos by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ let todos = [
     },
     { 
         _id: new ObjectID(),
-        text: 'Test to do text 2' 
+        text: 'Test to do text 2',
+        completed:  true,
+        completedAt: 444
     }
 ];
 
@@ -138,5 +140,45 @@ describe('DELETE /todos/:id', () => {
             .delete(`/todos/123`)
             .expect(404)
             .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        const hexId = todos[0]._id.toHexString();
+        const text = 'New to do text 1';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text 
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        const hexId = todos[1]._id.toHexString();
+        const text = 'Updated text 2';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done)
     });
 });
